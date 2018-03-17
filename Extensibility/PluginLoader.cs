57d016a7145c7520f65b6bdb7d514e86ba1d807e
Michael Sperber <m.sperber@programmersdigest.de>
2018-03-17 21:21:00 +0100

    Initial commit.

---
+++ Extensibility/PluginLoader.cs
@@ -0,0 +1,76 @@
+﻿using programmersdigest.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace programmersdigest.Extensibility {
+    internal class PluginLoader {
+        private const string PluginsAppDomainName = "PluginsDomain";
+
+        public Dictionary<string, List<string>> DiscoverPlugins(string searchPattern) {
+            // TODO NetCore does NOT implement AppDomains. Instead we are supposed to use AssemblyLoadContext, which
+            // in turn IS NOT PART OF netstandard 2.0 and NOT IMPLEMENTED in .NET 4.6.2.
+            // WTF???
+
+            AppDomain appDomain = null;
+            try {
+                appDomain = AppDomain.CreateDomain(PluginsAppDomainName);
+                var exeDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                var pluginAssemblyFiles = Directory.EnumerateFiles(exeDir, searchPattern, SearchOption.AllDirectories);
+
+                return LoadPluginAssemblies(appDomain, pluginAssemblyFiles);
+            }
+            finally {
+                if (appDomain != null) {
+                    AppDomain.Unload(appDomain);
+                }
+            }
+        }
+
+        private Dictionary<string, List<string>> LoadPluginAssemblies(AppDomain appDomain, IEnumerable<string> pluginAssemblyFiles) {
+            var cache = new Dictionary<string, List<string>>();
+            var exceptions = new List<Exception>();
+
+            foreach (var pluginAssemblyFile in pluginAssemblyFiles) {
+                try {
+                    // Append private path to temporary and main AppDomain.
+                    // Yes, AppendPrivatePath is obsolete, but netstandard 2.0
+                    // DOES NOT PROVIDE AN ALTERNATIVE! Thanks a bunch!
+#pragma warning disable CS0618
+                    appDomain.AppendPrivatePath(Path.GetDirectoryName(pluginAssemblyFile));
+                    AppDomain.CurrentDomain.AppendPrivatePath(Path.GetDirectoryName(pluginAssemblyFile));
+#pragma warning restore CS0618
+
+                    var name = AssemblyName.GetAssemblyName(pluginAssemblyFile);
+                    var assembly = appDomain.Load(name);
+
+                    DiscoverPluginTypes(assembly, ref cache);
+                }
+                catch (Exception ex) {
+                    exceptions.Add(ex);
+                }
+            }
+
+            if (exceptions.Any()) {
+                throw new AggregateException("One or more plugins could not be loaded.", exceptions);
+            }
+
+            return cache;
+        }
+
+        private void DiscoverPluginTypes(Assembly assembly, ref Dictionary<string, List<string>> cache) {
+            foreach (var type in assembly.GetTypes()) {
+                if (type.IsInterface || type.IsAbstract) {
+                    continue;
+                }
+
+                var pluginInterfaces = type.GetInterfaces();        // TODO This could become a rather huge interface map, no? Is this a problem?
+                foreach (var pluginInterface in pluginInterfaces) {
+                    cache.Add(pluginInterface.AssemblyQualifiedName, type.AssemblyQualifiedName);
+                }
+            }
+        }
+    }
+}
---
+++ Extensibility/PluginManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace programmersdigest.Extensibility {
+    public class PluginManager {
+        public delegate object CreatePluginInstanceCallback(Type implementation);
+
+        private const string PluginsAppDomainName = "PluginsDomain";
+
+        private string _searchPattern;
+        private Dictionary<string, List<string>> _pluginsCache;
+        private CreatePluginInstanceCallback _createPluginInstanceCallback;
+
+        public PluginManager() : this(@"*.plugin.dll") {
+        }
+
+        public PluginManager(string searchPattern) {
+            _searchPattern = searchPattern;
+            _createPluginInstanceCallback = DefaultCreatePluginInstanceCallback;
+        }
+
+        public void RegisterCreatePluginInstanceCallback(CreatePluginInstanceCallback callback) {
+            _createPluginInstanceCallback = callback;
+        }
+
+        public void DiscoverPlugins() {
+            var loader = new PluginLoader();
+            _pluginsCache = loader.DiscoverPlugins(_searchPattern);
+        }
+
+        public IEnumerable<Type> Find<TContract>() {
+            return Find(typeof(TContract));
+        }
+
+        public IEnumerable<Type> Find(Type contract) {
+            if (_pluginsCache == null) {
+                throw new InvalidOperationException($"The plugins cache is invalid. Please execute {nameof(DiscoverPlugins)} prior to retrieving plugins.");
+            }
+
+            if (contract == null) {
+                throw new ArgumentNullException(nameof(contract), "Contract must not be null");
+            }
+
+            lock (_pluginsCache) {
+                if (!_pluginsCache.TryGetValue(contract.AssemblyQualifiedName, out var implementations)) {
+                    implementations = new List<string>();
+                }
+
+                return implementations.Select(i => Type.GetType(i));
+            }
+        }
+
+        public IEnumerable<TContract> Load<TContract>() {
+            var implementations = Find<TContract>();
+            foreach (var implementation in implementations) {
+                yield return (TContract)_createPluginInstanceCallback(implementation);
+            }
+        }
+
+        private object DefaultCreatePluginInstanceCallback(Type implementation) {
+            return Activator.CreateInstance(implementation);
+        }
+    }
+}
